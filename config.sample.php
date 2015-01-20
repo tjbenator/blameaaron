6181d5646da864e9816a5303168e51f4fd74fe17
Travis Beck <tjbenator@gmail.com>
2015-01-20 04:28:18 +0000

    Sample Config

---
+++ config.sample.php
@@ -0,0 +1,11 @@
+<?php
+define('debug_mode', false);
+/**
+* MySQL Configuration
+* REQUIRED!
+**/
+define('DBDRIVER', 'mysql');
+define('DBNAME', 'bucket');
+define('DBUSERNAME', 'USERNAME'); //Preferably a user with only SELECT permissions
+define('DBPASSWORD', 'SECRET');
+define('DBHOSTNAME', 'localhost');
