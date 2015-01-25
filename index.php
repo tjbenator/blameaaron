d93d5c748ace50f8c1c16bd37fe46cfd50c99f9b
Travis Beck <tjbenator@gmail.com>
2015-01-25 14:15:16 +0000

    Added footer

--- index.php
+++ index.php
@@ -9,10 +9,10 @@ include('database.php');
 	<title>Blame Aaron</title>
 </head>
 <body>
-	<div id="header">
+	<header>
 		<a href="https://github.com/tjbenator/blameaaron"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png"></a>
 		<h1>Reasons to blame Aaron:</h1>
-	</div>
+	</header>
 	<div class="reasons">
 	<ul>
 	<?php
@@ -26,5 +26,8 @@ include('database.php');
 	?>
 	</ul>
 	</div>
+	<footer>
+		Also see <a href='http://aaronlogic.com'>AaronLogic.com</a>
+	</footer>
 </body>
 </html>
